afb984cb11a4e348d8e951a6001fb434ce83e1e8
Lukas <flaflo@ok.de>
2018-04-10 15:25:42 +0100

    Initial commit

---
+++ AdHoc.cmd
@@ -0,0 +1,54 @@
+::AdHoc.cmd Copyright (c) 2018 Flaflo
+::This file is distributed under the MIT License
+::Which can be read at "LICENSE.md" that should be shipped along file
+@echo off
+::If the first argument is not empty jump to the desired command
+if not "%1" == "" goto %1%
+goto usage
+:setup
+::Set default variable values
+set ssid=AdHoc
+set key=123456789
+::Request user input for variables, quotes added for visibility of spaces
+set /p "ssid=SSID: "
+set /p "key=Key: "
+::Write ini to file
+echo [AdHoc]>adhoc.ini&echo.ssid=%ssid%>>adhoc.ini&echo.key=%key%>>adhoc.ini
+netsh wlan set hostednetwork mode=allow ssid=%ssid% key=%key%
+goto end
+:reload
+::Setup ini default ini if not already exists and read into batch variables
+if not exist adhoc.ini echo [AdHoc]>adhoc.ini&echo.ssid=AdHoc>>adhoc.ini&echo.key=12346789>>adhoc.ini
+for /f "tokens=*" %%i in (ssid.ini) do set %%i
+::Apply config on hosted network
+echo Writing changes...
+netsh wlan set hostednetwork mode=allow ssid=%ssid% key=%key%
+echo In order to make changes take affect you need to restart the hosted network.
+goto end
+:status
+::Print general information about the network
+netsh wlan show hostednetwork | findstr /i status
+netsh wlan show hostednetwork | findstr /i " ssid "
+netsh wlan show hostednetwork setting=security | findstr /i /v /c:"--------------------------------"
+::Print all devices connected to the network
+echo Connected Devices
+::There may be cases where "192.168.137" is not the chosen private network IP and therefore needs adjustment here!
+arp -a | findstr /i 192.168.137 | findstr /i /v 192.168.137.255 | findstr /i /v /e 0xe 
+goto end
+:start
+::Start the network
+netsh wlan start hostednetwork
+goto end
+:stop
+::Stop the network
+netsh wlan stop hostednetwork
+goto end
+:usage
+::Print usage of this program
+echo AdHoc Usage:
+echo adhoc setup  - Runs a setup for the Ad Hoc network
+echo adhoc reload - Applys the config to the Ad Hoc network
+echo adhoc status - Shows the status of the Ad Hoc network
+echo adhoc start  - Starts the Ad Hoc network
+echo adhoc stop   - Stops the Ad Hoc network
+:end
